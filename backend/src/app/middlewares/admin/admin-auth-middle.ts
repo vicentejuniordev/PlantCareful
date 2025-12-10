ff8b2aba59561c23cf70ca43913c2f9804dd4324
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-12-10 08:40:31 -0300

    fix: token expiration exception handling

--- backend/src/app/middlewares/admin/admin-auth-middle.ts
+++ backend/src/app/middlewares/admin/admin-auth-middle.ts
@@ -1,21 +1,31 @@
 import type { NextFunction, Request, Response } from "express";
-import  jwt  from "jsonwebtoken";  ;
+import jwt from "jsonwebtoken";;
 
-const adminAuthMiddle = (req: Request, res: Response, next: NextFunction)=>{
+const adminAuthMiddle = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
+
     if (!authHeader) {
         return res.status(401).json({ message: 'Token não fornecido' });
     }
+
     const token = authHeader.split(' ')[1];
-    //Decodificar o token para saber o id do usuário
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decode;
-    
+
+
     if (!token) {
         return res.status(401).json({ message: 'Token inválido' });
     }
 
-    
+    try {
+        //Decodificar o token para saber o id do usuário
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decode;
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ message: 'Token expirado' });
+        }
+    }
+
+
     if (req.user.role !== 'admin') {
         return res.status(403).json({ message: 'Acesso negado: privilégios de administrador necessários' });
     }
