74bca669963e922709283dbae17d34bd58c6e9d5
vicentejuniordev <vicentejunior4522@gmail.com>
2026-01-12 09:23:55 -0300

    fix: remove jest because imcompatibility with tsx and change for vitest

--- backend/tests/get.test.ts
+++
@@ -1,9 +0,0 @@
-
-import testServer from "./jest.setup";
-
-describe("GET /users", () => {
-  it("should return 200 OK", async () => {
-    const response = await testServer.get("/users");
-    expect(response.status).toBe(200);
-  });
-});
